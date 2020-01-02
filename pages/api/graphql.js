cbfa0b2db36f3c12cad494866d8594d0805af0fb
Anton <arnellomutty2@gmail.com>
2020-01-02 10:06:57 +0200

    Created ApolloServer

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,3 +1,23 @@
-export default (req, res) => {
-  res.status(200).json({ test: "lklklklkl" });
+import { ApolloServer, gql } from "apollo-server-micro";
+
+const typeDefs = gql`
+  type Query {
+    sayHello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sayHello: () => "Hello up"
+  }
 };
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+export default apolloServer.createHandler({path: "/api/graphql"})
