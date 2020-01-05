c3afb0b3f1524d287982eeb4b603c007aa52a324
Anton <arnellomutty2@gmail.com>
2020-01-05 17:47:15 +0200

    Completed apollo client

--- services/apolloClient.tsx
+++ services/apolloClient.tsx
@@ -1,19 +1,67 @@
 import ApolloClient from 'apollo-boost'
+import Head from 'next/head'
 import { ApolloProvider } from '@apollo/react-hooks'
-import { NextPage } from 'next'
+import { NextPage, NextPageContext } from 'next'
 import fetch from 'isomorphic-fetch'
+import { getDataFromTree } from '@apollo/react-ssr'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
 export function withApollo(PageComponent: NextPage): NextPage {
   const WithApollo = (props: any) => {
-    const client = new ApolloClient({
-      uri: 'http://localhost:3000/api/graphql',
-      fetch,
-    })
+    const client = props.apolloClient || initApolloClient(props.apolloState)
     return (
       <ApolloProvider client={client}>
         <PageComponent {...props} />
       </ApolloProvider>
     )
   }
+
+  WithApollo.getInitialProps = async (ctx: NextPageContext) => {
+    const { AppTree } = ctx
+    const apolloClient = initApolloClient()
+    let pageProps = {}
+
+    if (PageComponent.getInitialProps) {
+      pageProps = await PageComponent.getInitialProps(ctx)
+    }
+
+    if (typeof window === 'undefined') {
+      if (ctx.res && ctx.res.finished) {
+        return pageProps
+      }
+
+      try {
+        await getDataFromTree(
+          <AppTree
+            pageProps={{
+              ...pageProps,
+              apolloClient,
+            }}
+          />
+        )
+      } catch (e) {
+        // tslint:disable-next-line:no-console
+        console.error(e)
+      }
+
+      Head.rewind()
+    }
+    const apolloState = apolloClient.cache.extract()
+    return {
+      ...pageProps,
+      apolloState,
+    }
+  }
+
   return WithApollo
+}
+
+const initApolloClient = (initState = {}) => {
+  const cache = new InMemoryCache().restore(initState)
+  const client = new ApolloClient({
+    uri: 'http://localhost:3000/api/graphql',
+    fetch,
+    cache,
+  })
+  return client
 }
