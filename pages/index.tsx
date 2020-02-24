32e01d0f05170cba862497e0355de6c01662e7da
Anton <arnellomutty2@gmail.com>
2020-02-24 16:23:36 +0200

    Added custom theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,10 @@ import { withApollo } from '../services/apolloClient'
 import { useQuery } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
 import { NextComponentType } from 'next'
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import { theme } from '../utils/theme'
+import Button from '@material-ui/core/Button'
+import Main from '../components/Main'
 
 const HELLO_QUERY = gql`
   query HelloQuery {
@@ -21,8 +25,12 @@ const Home: NextComponentType = () => {
 
   return (
     <>
-      <div>Go west</div>
-      <div>{data.sayHello}</div>
+      <MuiThemeProvider theme={theme}>
+        <Main />
+        <Button variant="contained">Default</Button>
+        <div>Go west</div>
+        <div>{data.sayHello}</div>
+      </MuiThemeProvider>
     </>
   )
 }
---
+++ utils/theme.ts
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#154353',
+      contrastText: '#FFFFFF',
+    },
+    secondary: {
+      main: '#00812C',
+      contrastText: '#FFFFFF',
+    },
+  },
+})
