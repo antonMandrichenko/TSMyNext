56a78b8301dec57ea8999299f5b4436acff66801
Anton <arnellomutty2@gmail.com>
2020-02-26 16:01:42 +0200

    Fixed some problems with styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import { withApollo } from '../services/apolloClient'
 import { useQuery } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
 import { NextComponentType } from 'next'
-import { MuiThemeProvider } from '@material-ui/core/styles'
-import { theme } from '../utils/theme'
+// import { MuiThemeProvider } from '@material-ui/core/styles'
+// import { theme } from '../utils/theme'
 import Button from '@material-ui/core/Button'
 import Main from '../components/Main'
 
@@ -24,12 +24,12 @@ const Home: NextComponentType = () => {
 
   return (
     <>
-      <MuiThemeProvider theme={theme}>
-        <Main />
-        <Button variant="contained">Default</Button>
-        <div>Go west</div>
-        <div>{data.sayHello}</div>
-      </MuiThemeProvider>
+      {/* <MuiThemeProvider theme={theme}> */}
+      <Main />
+      <Button variant="contained">Default</Button>
+      <div>Go west</div>
+      <div>{data.sayHello}</div>
+      {/* </MuiThemeProvider> */}
     </>
   )
 }
